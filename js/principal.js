15224c868213db9dd62594eae1b67912603e2e47
Barbara <barbararocha@alunos.utfpr.edu.br>
2022-08-10 15:47:11 -0300

    arrays e lops

--- js/principal.js
+++ js/principal.js
@@ -1,37 +1,45 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Bárbara Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+   
+    var paciente = pacientes[i];
 
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
+    var tdImc = paciente.querySelector(".info-imc");
 
-var pesoValido = true;
-var alturaValida = true;
+    var pesoValido = true;
+    var alturaValida = true;
 
-if (peso < 0 || peso >= 800) {
-    console.log("Peso inválido");
-    pesoValido = false;
-    tdImc.textContent = "Peso inválido!";
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+        
+    }
+
+  if (alturaValida && pesoValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if (altura < 0 || altura >= 3.00) {
-    console.log("Altura inválida");
-    alturaValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
 
-if (alturaValida && pesoValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}else {
-    tdImc.textContent = "Altura e/ou peso inválidos!";
-}
+
+
 
